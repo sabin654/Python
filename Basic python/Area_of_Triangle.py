d691ffb9b3024be6fb620b5c069d99c399c5c965
sabin654 <sabinbhandari86@gmail.com>
2022-11-01 22:17:27 +0545

    first commit

---
+++ Basic python/Area_of_Triangle.py
@@ -0,0 +1,7 @@
+
+x= 10
+y=5
+a=1/2 * x * y
+print("The area of Triangle is " ,a)
+
+
---
+++ Basic python/Swap.py
@@ -0,0 +1,11 @@
+
+x = 5
+y = 10
+
+temp = x
+x=y
+y= temp
+
+print("The swap value of x =",x)
+print("The swap value of y =",y)
+
