f806af3601174feabb30d9fb05dbe0bb0625b604
Sabin Bhandari <80193281+sabin654@users.noreply.github.com>
2022-11-05 19:53:19 +0545

    Add files via upload

---
+++ Basic python/largest_among_natural_number.py
@@ -0,0 +1,10 @@
+n1 = (input("Enter the first number: "))
+n2 = (input("Enter the second number: "))
+n3 = (input("Enter the third number: "))
+
+if n1>=n2 and n1>=n3:
+   print("the largest is",n1)
+elif n2>=n1 and n2>=n3:
+   print("the largest is",n2)
+else: print("the largest is",n3)
+
